e73c948668ff745a2f729b36bf997f9ef8e1a0fc
Sujeet <svontheweb@gmail.com>
2023-02-19 23:22:45 +0530

    redirect directly to MainActivity for loggedin user
    redirect func destroys 'getstarted activity' on call, after redirection

--- app/src/main/java/com/example/ktquixapp/LoginIntro.kt
+++ app/src/main/java/com/example/ktquixapp/LoginIntro.kt
@@ -4,12 +4,24 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginIntro : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_intro)
 
+        // if user is already logged in
+        val auth = FirebaseAuth.getInstance()
+        if(auth.currentUser != null) {
+            Toast.makeText(this, "Already logged in..", Toast.LENGTH_SHORT).show()
+            redirect("MAIN")
+        }
+
+        // if user is not logged in, first time visitors
         var btnGetStarted = findViewById<Button>(R.id.button)
         btnGetStarted.setOnClickListener {
             redirect("LOGIN")
